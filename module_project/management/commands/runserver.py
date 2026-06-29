8cb1b3ff4d127876abe047d0a443564638ca5571
fear <roma.bele.aleksandrovich@gmail.com>
2026-06-29 13:33:18 +0300

    redesign

--- module_project/management/commands/runserver.py
+++ module_project/management/commands/runserver.py
@@ -1,5 +1,5 @@
 from django.contrib.staticfiles.management.commands.runserver import Command as RunserverCommand
 
 class Command(RunserverCommand):
-    default_addr = '10.2.0.54'
-    default_port = '7350'
+    default_addr = '0.0.0.0'
+    default_port = '8000'
